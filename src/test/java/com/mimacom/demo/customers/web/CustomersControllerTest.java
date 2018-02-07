e3dd9d58fa717b2a75da94b5736756560e216d4c
Sandra Gomez <sandra.gomez@mimacom.com>
2018-02-07 18:16:06 +0100

    Step 1: first test with RestDocs

--- src/test/java/com/mimacom/demo/customers/web/CustomersControllerTest.java
+++ src/test/java/com/mimacom/demo/customers/web/CustomersControllerTest.java
@@ -4,17 +4,26 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mimacom.demo.customers.domain.Customer;
 import com.mimacom.demo.customers.domain.Gender;
 import com.mimacom.demo.customers.repository.CustomersRepository;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,7 +32,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(CustomersController.class)
 public class CustomersControllerTest {
 
-    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
@@ -32,9 +40,21 @@ public class CustomersControllerTest {
     @MockBean
     private CustomersRepository customersRepository;
 
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("build/generated-snippets");
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(this.restDocumentation))
+                .build();
+    }
+
     @Test
     public void testCreateOneCustomer() throws Exception {
-
 
         Customer customer = new Customer();
         customer.setAge(23);
@@ -44,15 +64,29 @@ public class CustomersControllerTest {
 
         when(this.customersRepository.save(any(Customer.class))).thenReturn(customer);
 
+        FieldDescriptor[] customerDescriptor = getCustomerFieldDescriptor();
+
         this.mockMvc.perform(post("/customers").content(this.objectMapper.writeValueAsString(customer))
                 .contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.firstName").value("Liam"))
-        .andExpect(jsonPath("$.lastName").isEmpty())
-        .andExpect(jsonPath("$.gender").value(Gender.MALE.name()))
-        .andExpect(jsonPath("$.phoneNumber").value("1119992"))
-        .andExpect(jsonPath("$.age").value(23));
+                .andExpect(jsonPath("$.firstName").value("Liam"))
+                .andExpect(jsonPath("$.lastName").isEmpty())
+                .andExpect(jsonPath("$.gender").value(Gender.MALE.name()))
+                .andExpect(jsonPath("$.phoneNumber").value("1119992"))
+                .andExpect(jsonPath("$.age").value(23))
+        .andDo(document("shouldCreateCustomer",
+                requestFields(customerDescriptor),
+                responseFields(customerDescriptor)));
+
 
     }
 
+    private FieldDescriptor[] getCustomerFieldDescriptor() {
+        return new FieldDescriptor[]{fieldWithPath("age").description("The age of the customer").type(Integer.class.getSimpleName()),
+                    fieldWithPath("firstName").description("The first name of the customer").type(String.class.getSimpleName()),
+                    fieldWithPath("gender").description("The gender of the customer (FEMALE or MALE)").type(Gender.class.getSimpleName()),
+                    fieldWithPath("phoneNumber").description("The cell phone number of the customer").type(String.class.getSimpleName()),
+                    fieldWithPath("id").description("The unique id of the customer").optional().type(Long.class.getSimpleName()),
+                    fieldWithPath("lastName").description("The last name of the customer").optional().type(String.class.getSimpleName())};
+    }
 }
